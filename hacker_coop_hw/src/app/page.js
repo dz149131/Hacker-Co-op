44089d500cef1ae67b4ab54aed107e8f4a85136a
Danny Zhang <dz149131@gmail.com>
2023-07-01 17:26:47 -0500

    Test

--- hacker_coop_hw/src/app/page.js
+++ hacker_coop_hw/src/app/page.js
@@ -1,17 +1,47 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+	const [message, setMessage] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const endpoint = 'https://www.hackercoop.dev/api/boop';
+			const headers = {
+				Authorization: `'Bearer' + process.env.SECRET_KEY`,
+				'Content-Type': 'application/json',
+			};
+			const body = { content: 'Message from wholemungbeans: ' + message };
+
+			await axios.post(endpoint, body, { headers });
+
+			console.log('Message sent successfully!');
+			setMessage('');
+		} catch (error) {
+			console.error('Failed to send message:', error);
+		}
+	};
+
+	const handleChange = (e) => {
+		setMessage(e.target.value);
+	};
 	return (
 		<main className="flex justify-center items-center min-h-screen">
 			<div className="container mx-auto flex flex-col items-center">
 				<h1 className="text-3xl font-medium border-b border-gray-400 mb-3 pb-1 text-white inline-block w-96">HOMEWORK</h1>
-				<form className="flex flex-col items-center" id="messageForm">
+				<form className="flex flex-col items-center" id="messageForm" onSubmit={handleSubmit}>
 					<input
 						className="w-96 px-3 py-2 rounded-lg bg-white text-gray-800 placeholder-gray-400 focus:outline-none focus:ring focus:border-blue-500"
 						type="text"
 						id="messageInput"
 						placeholder="Enter a message..."
+						value={message}
+						onChange={handleChange}
 					></input>
 
 					<button
